ae27f9718c4de3a3ca5d9d1fbb8efd29a55a14b8
Doğancan Cedetaş <dogancancedetas@hotmail.com>
2021-10-12 12:12:27 +0300

    MAP katmanı tamamlandı

--- Project.ENTITIES/Models/Order.cs
+++ Project.ENTITIES/Models/Order.cs
@@ -11,8 +11,8 @@ namespace Project.ENTITIES.Models
         public string ShippedAddress { get; set; }
         public decimal TotalPrice { get; set; }
 
-        //Sipariş işlemlerinin içerisindeki bilgileri daha rahat yakalamak adına açtığımız property'lerden bir tanesi TotalPrice'tır. Yalnız burada çok dikkatli olmanız gerekir gerçekten size hız kazandıracak bir durum varsa bu bilgileri ek olarak buraya almanız gerekir. Aynı zamanda bu durum abartılmamalıdır. İlgili yapının tüm verileri asla bu sınıfa komple koyulmamalıdır. Sadece spesifik parçalar alınmalıdır
-
+        //Sipariş işlemlerinin içerisindeki bilgileri daha rahat yakalamak adına açtığımız property'lerden bir tanesi TotalPrice'tır. Yalnız burada çok dikkatli olmanız gerekir gerçekten size hız kazandıracak bir durum varsa bu bilgileri ek olarak buraya almanız gerekir. Aynı zamanda bu durum abartılmamalıdır. İlgili yapının tüm verileri asla bu sınıfa komple koyulmamalıdır. Sadece spesifik parçalar alınmalıdır.
+       
         public string UserName { get; set; }
         public string Email { get; set; }
         public int? AppUserID { get; set; }
