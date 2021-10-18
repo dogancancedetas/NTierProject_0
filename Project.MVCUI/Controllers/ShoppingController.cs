271cdba36c63540a38e2c8dc642ab50667302986
Doğancan Cedetaş <dogancancedetas@hotmail.com>
2021-10-18 11:03:14 +0300

    product list tamamlandı

--- Project.MVCUI/Controllers/ShoppingController.cs
+++ Project.MVCUI/Controllers/ShoppingController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PagedList;
+using Project.BLL.DesignPatterns.GenericRepository.ConcRep;
+using Project.MVCUI.VMClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,39 @@ namespace Project.MVCUI.Controllers
 {
     public class ShoppingController : Controller
     {
-        // GET: Shopping
-        public ActionResult Index()
+
+        OrderRepository _oRep;
+        ProductRepository _pRep;
+        CategoryRepository _cRep;
+        OrderDetailRepository _odRep;
+
+        public ShoppingController()
         {
-            return View();
+            _oRep = new OrderRepository();
+            _odRep = new OrderDetailRepository();
+            _pRep = new ProductRepository();
+            _cRep = new CategoryRepository();
+        }
+
+        // GET: Shopping
+        public ActionResult ShoppingList(int? page, int? categoryID) //nullable int vermemizin sebebi aslında buradaki int'in kaçıncı sayfada olduğumuzu temsil edecek olmasıdır. Ancak birisi direkt alışveriş sayfasına ulaştığında hangi sayfada olduğu verisi olamayacağından dolayı bu şekilde de (sayfa numarası gönderilmeden) bu action'ın çalışabilmesini istiyoruz
+        {
+            //string a = "Mehmet";
+            //string b = a ?? "Çağrı"; //a null ise b'ye Çağrı değerini at ama a'nın değeri null değilse b'ye a'yı at.
+
+            //page??1
+
+            //page?? ifadesi page null ise demektir
+
+            PaginationVM pavm = new PaginationVM
+            {
+                PagedProducts = categoryID == null ? _pRep.GetActives().ToPagedList(page ?? 1, 9) : _pRep.Where(x => x.CategoryID == categoryID).ToPagedList(page ?? 1, 9),
+                Categories = _cRep.GetActives()
+                
+            };
+            if (categoryID != null) TempData["catID"] = categoryID;
+
+            return View(pavm);
         }
     }
 }
